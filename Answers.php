715a0646210b8e17e068cc314054082a329a76c7
Arnaldogonzalez <agonzalez@fvi.edu>
2017-12-07 15:48:20 -0500

    PHP final exam

---
+++ Answers.php
@@ -0,0 +1,59 @@
+Ques #1:
+B) 5
+
+Ques #2:
+C) PHP interfaces to a number of relational databases such as MySQL, Oracle and Sybase but the interface diﬀers in each case.
+
+Ques #3:
+B) /phptutor/index.php
+
+Ques #4:
+B) Yes​ PHP supports two diﬀerent types of regular expressions: POSIX​extended and Perl​ Compatible Regular Expressions (PCRE).
+
+Ques #5:
+A) Larry Wall  // Crador de PERL
+B) Rasmus Lerdorf // Creador de PHP
+C) James Gosling  // Creador de JAVA
+D) Guido Van Rossum  // Crador de PYTHON
+
+Ques #6:
+A) $s1 + $s2
+
+Ques #7:
+
+
+Ques #8:
+D) Global variables
+
+Ques #9:
+B) require()
+
+Ques #10:
+C) Null
+
+Ques #11:
+C) shows the IP address of the visitor
+
+Ques #13:
+B) True
+
+Ques #14:
+A) database name
+
+Ques #15:
+B) function parameter 
+
+Ques #16:
+A) bool function_exists(functionname)
+
+Ques #17:
+A) True 
+
+Ques #18:
+A) Yes
+
+Ques #19:
+B) $ 
+
+Ques #20:
+B) Christmas
